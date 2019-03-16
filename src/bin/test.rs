c359a28d7c7ae4e6b6a01efcff2f1c7aadbbe581
Benoit Eudier <benoit.eudier@gmail.com>
2019-03-16 17:00:06 +0900

    setup the message sending from server

---
+++ src/bin/test.rs
@@ -0,0 +1,12 @@
+use twgraph::net::Server;
+use std::error::Error;
+use tokio::prelude::*;
+
+fn main() -> Result<(), Box<Error>> {
+    
+    let server = Server::connect(8080, 2)?;
+
+    tokio::run(server.map_err(|err| println!("Error happened: {:?}", err)));
+    Ok(())
+}
+
--- src/net/server.rs
+++ src/net/server.rs
@@ -3,41 +3,83 @@ use std::io;
 use tokio::prelude::*;
 use tokio::net::{UdpFramed, UdpSocket};
 use tokio_codec::BytesCodec;
+use futures::sync::mpsc;
 use tokio::prelude::stream::{SplitSink, SplitStream};
-use bytes::BytesMut;
-
-const CLIENT_NB: usize = 8;
+use bytes::{Bytes, BytesMut};
 
 use super::NetworkError;
+use futures::try_ready;
 
 pub struct Server {
     clients: Vec<SocketAddr>,
-    sink: SplitSink<UdpFramed<BytesCodec>>,
+    max_clients: usize,
+    tx: mpsc::Sender<(Bytes, SocketAddr)>,
     stream: SplitStream<UdpFramed<BytesCodec>>,
+
+    // buffer of messages to send :)
+    
 }
 
 impl Server {
-    
-    pub fn connect(port: usize) -> Result<Self, Box<std::error::Error>> {
-        let mut clients = Vec:: new();
-        clients.reserve_exact(CLIENT_NB);
 
-        let addr = format!("127.0.0.1:{}", port).parse()?;
-        let socket = UdpSocket::bind(&addr)?;
+    pub fn connect(port: usize,
+                   max_clients: usize) -> 
+        Result<Box<Future<Item = (), Error = io::Error> + Send>, Box<std::error::Error>> {
 
-        let (sink, stream) = UdpFramed::new(socket, BytesCodec::new())
-            .split();
 
-        Ok(Server {
-            clients,
-            sink,
-            stream,
-        })
-    }
+
+            let mut clients = Vec:: new();
+            clients.reserve_exact(max_clients);
+
+            let addr = format!("127.0.0.1:{}", port).parse()?;
+            let socket = UdpSocket::bind(&addr)?;
+
+            let (sink, stream) = UdpFramed::new(socket, BytesCodec::new())
+                .split();
+
+
+            // The sink will be in its own tasks :) And receive the messages
+            // to send via the channel.
+            let (tx, rx) = mpsc::channel(0); // TODO what is the buffer size doing?
+            let rx: Box<Stream<Item = (Bytes, SocketAddr), Error = io::Error> + Send> = Box::new(rx.map_err(|e| panic!("{:?}", e)));
+
+            let forward_msg = rx
+                .forward(Box::new(sink))
+                .then(|result| {
+                    if let Err(e) = result {
+                        println!("Failed to write to socket: {:?}", e);
+                    }
+                    Ok(())
+                });
+
+
+            let server = Server {
+                clients,
+                max_clients,
+                tx,
+                stream,
+            };
+
+            let fut = Box::new(future::lazy(|| {
+                tokio::spawn(forward_msg);
+                tokio::spawn(server.map_err(|_| println!("Got an error ..")));
+                future::ok(())
+            }));
+
+            Ok(fut)
+        }
 
 
     fn handle_new_client(&mut self, addr: SocketAddr, msg: BytesMut) {
-        println!("New client({:?} with message {:?}", addr, msg)
+        println!("New client({:?} with message {:?}", addr, msg);
+
+        if self.clients.len() < self.max_clients {
+            self.tx.start_send((msg.into(), addr));
+            self.clients.push(addr);
+            println!("Connection accepted") 
+        } else {
+            println!("Connection refused");
+        }
     }
 }
 
@@ -52,7 +94,7 @@ impl Future for Server {
 
         // Will check for update from clients.
         while let Async::Ready(data) = self.stream.poll()? {
-            
+
             match data {
                 Some((message, addr)) => {
 
@@ -63,7 +105,7 @@ impl Future for Server {
                     for client in &self.clients {
 
                         if *client == addr {
-
+                            println!("Got a message from {:?}", client);
                             found = true;
                             break;
                         }
@@ -82,7 +124,9 @@ impl Future for Server {
 
 
         // Send available data to clients.
-        
+        try_ready!(self.tx.poll_complete().map_err(|e| {
+            io::Error::new(io::ErrorKind::Other, e.to_string())   
+        }));
         Ok(Async::NotReady)
     }
 }
