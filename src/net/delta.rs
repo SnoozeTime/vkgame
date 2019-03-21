725261cea72adfdae481f108ad4cc1ee4e644c61
Benoit Eudier <benoit.eudier@gmail.com>
2019-03-21 17:48:29 +0900

    add test + refactor a bit

--- src/net/delta.rs
+++ src/net/delta.rs
@@ -1,11 +1,3 @@
-use crate::ecs::{ECS, components::{
-    TransformComponent,
-    ModelComponent,
-}
-};
-use cgmath::InnerSpace;
-
-const EPSILON: f32 = 0.00001;
 // Utilities to extract state changes from two
 // ECS.
 //
@@ -15,8 +7,35 @@ const EPSILON: f32 = 0.00001;
 //
 // For example, if the object has moved a bit, send the delta. If the mesh has morphed, send it as
 // well.
+use crate::ecs::{
+    ECS, 
+    components::{
+        TransformComponent,
+        ModelComponent,
+    }
+};
+use cgmath::InnerSpace;
+
+const EPSILON: f32 = 0.00001;
+
+// Compute change between two ECS
+//
+// What kind of action:
+// - UPDATE entity (if update non-existing, should create it)
+// - DEALLOCATE entity
+// - REMOVE ALL (for example, when changing the level)
 fn compute_delta(old: &ECS, current: &ECS) {
 
+    // Get all live entities in current
+    //
+    // Iterate
+    //
+    // If can find same entity in old, compute the difference.
+    // If cannot, delete if existing but old generation and compute difference with nothing -> get current state
+
+}
+
+fn apply_delta(ecs: &mut ECS) {
 
 }
 
@@ -68,4 +87,46 @@ fn compute_model_delta(old_model: &ModelComponent,
     };
 
     (delta_mesh, delta_texture)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cgmath::Vector3;
+
+    #[test]
+    fn delta_transform_test() {
+        let current = TransformComponent {
+            position: Vector3::new(2.3, -12.0, 2.0),
+            rotation: Vector3::new(0.0, 1.0, 0.0),
+            scale: Vector3::new(0.0, 0.0, 0.0),
+        };
+        let old = TransformComponent {
+            position: Vector3::new(0.0, -12.0, 0.0),
+            rotation: Vector3::new(0.0, 1.0, 0.0),
+            scale: Vector3::new(1.0, 1.0, 1.0),
+        };
+
+        let (mpos, mrot, mscale) = compute_transform_delta(&old, &current);
+        assert_eq!(Some([2.3, 0.0, 2.0]), mpos);
+        assert_eq!(None, mrot);
+        assert_eq!(Some([-1.0, -1.0, -1.0]), mscale);
+    }
+
+    #[test]
+    fn delta_model_test() {
+        let current = ModelComponent {
+            mesh_name: "current".to_string(),
+            texture_name: "old".to_string(),
+        };
+
+        let old = ModelComponent {
+            mesh_name: "old".to_string(),
+            texture_name: "old".to_string(),
+        };
+
+        let (mmesh, mtext) = compute_model_delta(&old, &current);
+        assert_eq!(Some("current".to_string()), mmesh);
+        assert_eq!(None, mtext);
+    }
 }
--- src/net/server.rs
+++ src/net/server.rs
@@ -15,6 +15,7 @@ use std::sync::mpsc as stdmpsc;
 
 use crate::sync::SharedDeque;
 use super::NetworkError;
+use crate::ecs::ECS;
 
 pub fn start_serving(port: usize)
     -> Result<(SharedDeque<protocol::NetMessage>, stdmpsc::Sender<protocol::NetMessage>),
@@ -108,3 +109,103 @@ fn read_channel(mut tx: futmpsc::Sender<(Bytes, SocketAddr)>,
     }
 }
 
+/// The network system is the ECS system that will be called in the main loop.
+/// it should provide events and allow to send messages.
+pub struct NetworkSystem {
+    from_clients: SharedDeque<protocol::NetMessage>,
+    to_clients: std::sync::mpsc::Sender<protocol::NetMessage>,
+
+    my_clients: Vec<SocketAddr>,
+    max_clients: usize,
+}
+
+
+impl NetworkSystem {
+
+    pub fn new(port: usize,
+               max_clients: usize) -> Self {
+        let (from_clients, to_clients) = start_serving(port).unwrap();
+
+        let my_clients = vec![];
+
+        Self {
+            //server,
+            to_clients,
+            from_clients,
+            my_clients,
+            max_clients,
+        }
+    }
+
+
+    /// Will get the latest events that were sent to the server
+    /// For example, player commands and so on.
+    pub fn poll_events(&mut self) {
+
+        let events = self.from_clients.drain();
+
+        for ev in events {
+
+            if let protocol::NetMessageContent::ConnectionRequest = ev.content {
+                self.handle_connection_request(ev.target);
+            }
+
+            debug!("Network system received {:?}", ev);
+        }   
+    }
+
+
+    /// This will send the current state to all clients.
+    pub fn send_state(&mut self, ecs: &mut ECS) {
+
+        for c in self.my_clients.iter() {
+            let msg = protocol::NetMessage { 
+                content: protocol::NetMessageContent::Text(String::from("Bonjour")),
+                target: *c
+            };
+            self.to_clients.send(msg);
+        }
+    }
+
+    /// This is called when a ConnectionRequest message is received
+    /// It will reply with either connection accepted or connection refused
+    /// and add the client to our map of clients.
+    ///
+    /// If a client is already in the map, it should reply connection
+    /// accepted. The reason is that the connection acception message
+    /// might have been lost so the client thinks it is still trying to connect
+    fn handle_connection_request(&mut self, addr: SocketAddr) {
+
+        info!("Handle new connection request from {}", addr);
+
+        let to_send = {
+            if self.has_client(addr) {
+                info!("Client was already connected, resend ConnectionAccepted");
+                protocol::NetMessageContent::ConnectionAccepted
+            } else { 
+                // in that case we need to find an empty slot. If available,
+                // return connection accepted.
+                if self.my_clients.len() < self.max_clients {
+                    info!("Client connected");
+                    self.my_clients.push(addr);
+                    protocol::NetMessageContent::ConnectionAccepted
+                } else {
+                    info!("Too many clients connected, send ConnectionRefused");
+                    protocol::NetMessageContent::ConnectionRefused
+                }
+            }
+        };
+
+        self.to_clients.send(protocol::NetMessage {
+            target: addr,
+            content: to_send,
+        });
+    }
+
+
+    fn has_client(&self, addr: SocketAddr) -> bool {
+        self.my_clients.iter().any(|&client| {
+            client == addr
+        })
+    }
+}
